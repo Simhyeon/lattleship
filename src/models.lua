62cb9a785584bbe6211fbe6d175bc22048abe5e7
simoncreek <simoncreek@tutanota.com>
2022-02-18 19:38:46 +0900

    Field state related methods

--- src/models.lua
+++ src/models.lua
@@ -1,20 +1,88 @@
-local ships_state = {
-	-- 2 by 2 matrix with ENUM as value
+local utils = require("./utils")
+
+-- Enum BlockState
+local block_state = {
+	blank = 1,
+	occupied = 2,
+	attacked = 3,
 }
+
+local FieldState = {
+	row = 10,
+	col = 10,
+	field = {},
+}
+
+---- <FieldStateMethod>
 
 -- TODO
 -- Implement method of
 --
 -- generate_ships
--- attack
+
+-- TODO
+-- Rather than empy table, make a whole playable table
+function FieldState:new()
+	local o = {}
+	setmetatable(o,self)
+	self.__index = self
+	-- Assign new empty field
+	o.field = utils.empty_2d_array(
+		self.row,
+		self.col,
+		block_state.blank
+	)
+	return o
+end
+
+-- Return true if succeed
+-- Return false if failed
+-- Retrun nil if indexing is out of range
+function FieldState:attack(row,col)
+	if row < 1 or row > self.row or col < 1 or col > self.col then
+		print("Row or column is out of range")
+		return nil
+	end
+
+	-- Attack only if occupied
+	-- row col is inversed in 2d array implementation
+	if self.field[col][row] == block_state.occupied then
+		self.field[col][row] = block_state.attacked
+		return true
+	end
+
+	return false
+end
+
+function FieldState:index(row,col)
+	if row < 1 or row > self.row or col < 1 or col > self.col then
+		print("Row or column is out of range")
+		return nil
+	end
+	-- row col is inversed in 2d array implementation
+	return self.field[col][row]
+end
+
+function FieldState:occupy(row,col)
+	if row < 1 or row > self.row or col < 1 or col > self.col then
+		print("Row or column is out of range")
+		return nil
+	end
+	-- row col is inversed in 2d array implementation
+	self.field[col][row] = block_state.occupied
+	return true
+end
+
+---- </FieldStateMethod>
 
 local game_state = {
 	["id"]= "",
-	["player"] = ships_state,
-	["computer"] = ships_state,
+	["player"] = FieldState:new(),
+	["computer"] = FieldState:new(),
 }
 
 return {
-	user_state = ships_state,
+	FieldState = FieldState,
+	user_state = FieldState,
 	game_state = game_state,
 }
