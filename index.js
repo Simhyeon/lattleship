56f16afc0012e44a7769172a5e059468836ee57d
simoncreek <simoncreek@tutanota.com>
2022-02-21 05:47:01 +0900

    Backup

--- index.js
+++ index.js
@@ -1,3 +1,59 @@
-function start_game() {
-	// Send start request
+function onLoad() {
+	const startBtn = document.getElementById("start-button")
+	startBtn.addEventListener('click', start_game)
+	console.log("ONLOAD")
 }
+
+// Returned value is --
+//local ActionResult = {
+	//game_state = GameState,
+	//new_player_blocks = {},
+	//new_computer_blocks = {},
+//}
+
+function start_game() {
+	console.log("Start game")
+	const myField = document.getElementById("player-field")
+	const computerField = document.getElementById("computer-field")
+	fetch('/start')
+		.then(function(response) {
+			return response.json();
+		})
+		.then(function(json) {
+			console.log(json)
+			createField(
+				myField,
+				json.player
+			)
+			createField(
+				computerField,
+				json.computer
+			)
+		});
+}
+
+// TODO
+// Rather than maullay appending elements,
+// Consider using insertAdjacent for simple dom parsing and addition
+function createField(elem, blocks) {
+	let htmlString = ""
+	for (let i = 0, len = blocks[1].length; i < len; i++) {
+		htmlString += `<div class="row">`
+		for (let j = 0, len = blocks.length; j < len; j++) {
+			htmlString += `<div class="block ${blocks[j][i]}"></div>`
+		}
+		htmlString += "</div>"
+	}
+	elem.insertAdjacentHTML('beforeend',htmlString)
+}
+
+// TODO
+// Demo representation of final result
+//<div id="target">
+	//<div class="row">
+		//<div class="block"></div>
+		//<div class="block"></div>
+		//<div class="block"></div>
+		//<div class="block"></div>
+	//</div>
+//</div>
