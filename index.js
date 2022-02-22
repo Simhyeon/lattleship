c4b62bb6e8f4f443a1f8259628934076009109f5
simoncreek <simoncreek@tutanota.com>
2022-02-22 20:49:15 +0900

    New refreshing interval logic

--- index.js
+++ index.js
@@ -3,48 +3,69 @@ $pause(true) // Rad macro operation
 let connectionId  = ""
 let onStart       = false
 let onRequest     = false
-let lastTime      = null
+let discInterval  = null // Disconnect Interval
 const BlockStates = ["blank", "occupied", "attacked", "cleared"]
+
+function resetEnv() {
+	// Reset connection id
+	connectionId = "" 
+	onStart      = false
+	onRequest    = false
+	clearInterval(discInterval)
+}
+
+// tv is bool
+function setBlur(tv) {
+	const elem = document.getElementById("panel")
+	if (tv) { // true
+		elem.classList.add("blurred")
+	} else {  // false
+		elem.classList.remove("blurred")
+	}
+}
+
+function disableGame(alertMsg) {
+	resetEnv()
+	setBlur(true)
+	window.alert(alertMsg)
+}
 
 function onLoad() {
 	const startBtn = document.getElementById("start-button")
 	startBtn.addEventListener('click', startGame)
 }
 
-// Returned value of /pick api --
-//local ActionResult = {
-	//winner = "player" | "computer",
-	//player = {
-	//		state = STRING
-	//		row   = NUM
-	//		col   = NUM
-	//},
-	//computer = <Same with new player>,
-//}
-
 function isBusy() {
 	return onRequest || onStart
 }
 
-// TODO
-// Not only return boolean but make it to show a pop up menu
-function checkDisconnected(current_time) {
-	if ( (current_time - lastTime) / 1000  >= 60 ) {
-		return true
-	} else {
-		return false
-	}
+// This sets interval
+function refreshConnection() {
+	discInterval = setInterval(() => {
+		console.log("Refreshing connection")
+		fetch("/refresh", {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+			},
+			body: new URLSearchParams({
+				'id': connectionId,
+			})
+		}).catch(error => {
+			disableGame(`Failed to connect to server\nErr : ${error}`)
+		})
+	}, 30000);
 }
 
 function startGame() {
 	if (isBusy()) return;
-	console.log("Start game")
+	console.log("Start game");
 	// Clear everything before assigning field
-	clearField()
-	const myField = document.getElementById("player-field")
-	const computerField = document.getElementById("computer-field")
-	onStart = true
-	lastTime = Date.now();
+	clearField();
+	setBlur(false);
+	const myField = document.getElementById("player-field");
+	const computerField = document.getElementById("computer-field");
+	onStart = true;
 	fetch('/start')
 		.then((response) => {
 			return response.json();
@@ -65,7 +86,11 @@ function startGame() {
 
 			// End start flow
 			onStart = false
+		}).catch(error => {
+			disableGame(`Failed to connect to server\nErr : ${error}`)
 		});
+
+	refreshConnection();
 }
 
 function clearField() {
@@ -101,6 +126,20 @@ function addBtnEventListener() {
 	})
 }
 
+// Returned value of /pick api --
+//local ActionResult = {
+	//winner = "player" | "computer",
+	//player = {
+	//		state = STRING
+	//		row   = NUM
+	//		col   = NUM
+	//},
+	//computer = <Same with new player>,
+//}
+
+// Send /pick POST request with params
+// TODO
+// Check if server timeout is returned
 function attackBlock(event) {
 	if (connectionId == "") return
 	if (isBusy()) return;
@@ -109,6 +148,7 @@ function attackBlock(event) {
 		// Don't request if previous request is on load
 		onRequest = true;
 
+		// Send request
 		fetch("/pick", {
 			method: 'POST',
 			headers: {
@@ -122,25 +162,24 @@ function attackBlock(event) {
 		}).then((response) => {
 			return response.json();
 		}).then((json) => {
-			console.log(json)
-			if (json.winner == undefined) { // Either undefined or winner
-				const pBlock   = json.player
-				const cBlock   = json.computer
-				if (pBlock != undefined ) {
-					const block = document.querySelector(`#player-field .b-${pBlock.row}${pBlock.col}`)
-					block.classList.remove(...BlockStates)
-					block.classList.add(pBlock.state)
-				}
-				if (cBlock != undefined ) {
-					const block = document.querySelector(`#computer-field .b-${cBlock.row}${cBlock.col}`)
-					block.classList.remove(...BlockStates)
-					block.classList.add(cBlock.state)
-				}
-			} else {
-				// Game end
-				connectionId = "" // Reset connection id
-				gameEnd(json.winner)
+			const pBlock   = json.player
+			const cBlock   = json.computer
+			if (pBlock != undefined ) {
+				const block = document.querySelector(`#player-field .b-${pBlock.row}${pBlock.col}`)
+				block.classList.remove(...BlockStates)
+				block.classList.add(pBlock.state)
 			}
+			if (cBlock != undefined ) {
+				const block = document.querySelector(`#computer-field .b-${cBlock.row}${cBlock.col}`)
+				block.classList.remove(...BlockStates)
+				block.classList.add(cBlock.state)
+			}
+			if (json.winner != undefined) { // Either undefined or winner
+				// Game end
+				gameEnd(json.winner)
+			} 
+		}).catch(error => {
+			disableGame(`Failed to connect to server\nErr : ${error}`)
 		});
 
 		// End reqeust flow
@@ -148,10 +187,11 @@ function attackBlock(event) {
 	}
 }
 
-// winner is string
+// winner is a string
 function gameEnd(winner) {
-	// TODO
-	// Show pop up
+	resetEnv()
+	setBlur(true)
+	window.alert(`${winner} won.`)
 }
 
 $pause(false) // Rad macro operation
